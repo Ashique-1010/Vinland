ccf65d5fbe1db774954fd4a0478071aaffc64c55
Ashique A B <ab.ashique10@gmail.com>
2024-11-23 18:26:59 +0530

    added user authentication

---
+++ backend/app/services/auth.py
@@ -0,0 +1,47 @@
+from sqlalchemy.orm import Session
+from app.models.user import User
+from app.schemas.user import UserCreate
+from app.core.security import get_password_hash, verify_password
+from app.config import get_settings
+from fastapi import HTTPException
+from jose import jwt
+from datetime import datetime, timedelta
+
+settings = get_settings()
+
+def get_user_by_email(db: Session, email: str):
+    return db.query(User).filter(User.email == email).first()
+
+def create_user(db: Session, user: UserCreate):
+    if get_user_by_email(db, user.email):
+        raise HTTPException(status_code=400, detail="Email already registered")
+
+    hashed_password = get_password_hash(user.password)        
+    db_user = User(
+        email = user.email,
+        password = user.password,
+        hashed_password = hashed_password
+    )
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db.user
+
+def authenticate_user(db: Session, email: str, password: str):
+    user = get_user_by_email(db, email)
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp":expire})
+    encoded_jwt = jwt.encode(
+        to_encode,
+        # settings.SECRET_KEY
+        algorithm=settings.ALGORITHM
+    )
+    return encoded_jwt
